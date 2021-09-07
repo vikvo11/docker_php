2f13d080ef185306c219aa2413f6b79276bdf151
vikvo11 <34969699+vikvo11@users.noreply.github.com>
2021-09-07 15:02:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
 
+echo "<h1>Hello from k8s </h1><br>";
+echo "SErver IP Address is: $ip_server", "<br><p>";
+echo "Made by <font color=blue>VikVo";
+
+?>
